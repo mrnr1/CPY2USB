645a1c4582ddfa33723fb1b7c1dd2dd357cda804
Jeff Fabish <electricnetworks@gmail.com>
2019-03-28 10:41:31 -0500

    Add files via upload

---
+++ Cpy2Usb/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Cpy2Usb
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
